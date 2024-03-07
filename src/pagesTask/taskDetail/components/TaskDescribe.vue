8e764ef2cb0962deb70be4a4dc5fc08bb0ce4ad5
gitJuan <dailijuan_juan@163.com>
2024-03-07 19:48:24 +0800

    fix: #5885 获取到任务内容后重置高度

--- src/pagesTask/taskDetail/components/TaskDescribe.vue
+++ src/pagesTask/taskDetail/components/TaskDescribe.vue
@@ -1,24 +1,32 @@
 <template>
   <view class="section-title hx-mb-[10px]"> 任务描述 </view>
   <ProReadMore
+    ref="readMoreRef"
     show-height="313px"
     :toggle="true"
     close-text="查看更多"
     class="hx-mb-[24px]"
   >
-    <view> {{ data.content }} </view>
+    <view> {{ data.content }}</view>
   </ProReadMore>
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { PropType, ref, watch } from 'vue';
 
 import { ITaskDetail } from '@/api/fe/wechat/task';
 
-defineProps({
+const props = defineProps({
   data: {
     type: Object as PropType<ITaskDetail>,
     default: () => ({})
   }
 });
+const readMoreRef = ref();
+watch(
+  () => props.data,
+  () => {
+    readMoreRef.value.init();
+  }
+);
 </script>
