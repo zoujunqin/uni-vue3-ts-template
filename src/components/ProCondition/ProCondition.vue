7c5711595cd10f0584e9313a45047a8aecdb54ea
gitJuan <dailijuan_juan@163.com>
2024-06-19 11:26:18 +0800

    fix(v1.0.0): ProCondition组件修改

--- src/components/ProCondition/ProCondition.vue
+++ src/components/ProCondition/ProCondition.vue
@@ -1,6 +1,7 @@
 <template>
-  <view class="hx-flex hx-items-end hx-p-[11px_16px]" @click="handleClick">
+  <view class="hx-flex hx-items-end hx-p-[11px_16px]">
     <text
+      @click="handleClick"
       class="hx-text-[14px] hx-font-[400] hx-leading-[21px] hx-text-text-color-content hx-mr-[3px]"
       :class="activeTextClass"
     >
