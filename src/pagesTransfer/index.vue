9cae8187247f16f053dee250a27dba854b4a04e8
gitJuan <dailijuan_juan@163.com>
2024-08-30 17:45:27 +0800

    fix(v1.3.0): cr

--- src/pagesTransfer/index.vue
+++ src/pagesTransfer/index.vue
@@ -7,15 +7,15 @@ import { onLoad } from '@dcloudio/uni-app';
 import { loginPagePath } from '@/constant/pagePath';
 import { useUserStore } from '@/pinia/modules/user';
 import { sceneCodeMap } from '@/sceneCode';
-const pages = getCurrentPages();
 
 onLoad(option => {
+  const pages = getCurrentPages();
   wx.onAppRoute(res => {
     const { openType } = res;
     if (openType === 'navigateBack') {
       let url = '/pages/portal/index';
       if (pages.length > 1) {
-        url = pages[pages.length - 2].route;
+        url = `/${pages[pages.length - 2].route}`;
       }
       uni.redirectTo({ url });
     }
