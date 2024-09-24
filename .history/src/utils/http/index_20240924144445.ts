a6466e04c57160010b5253bb1fafbf7c806fd4d3
dailijuan <dailijuan_juan@163.com>
2024-09-24 14:48:10 +0800

    chore(v1.5.0): http

--- .history/src/utils/http/index_20240924144445.ts
+++ .history/src/utils/http/index_20240924144445.ts
@@ -29,12 +29,12 @@ const httpNoMessage = ['/fe/wechat/worker_protocol/sign'];
 
 // const baseURL = 'http://218.104.230.173:17054'; // 开发环境
 // const baseURL = 'https://localdev-hro-api.fjhxrl.com'; // 测试环境
-const baseURL = 'https://localtest-hro-api.fjhxrl.com'; // 测试环境
+// const baseURL = 'https://localtest-hro-api.fjhxrl.com'; // 测试环境
 // const baseURL = 'https://hro-beta-gateway.fjhxrl.com'; // 预生产环境
 // const baseURL = 'http://192.168.3.73:8100'; // 林伦 wifi
 // const baseURL = 'http://192.168.117.87:8100'; // 林伦 网线
 // const baseURL = 'http://192.168.117.86:8100'; // 大立
-// const baseURL = 'http://172.16.114.70:8100'; //
+const baseURL = 'http://172.16.114.70:8100'; //
 
 const { DEV, VITE_BASE_URL } = import.meta.env;
 // 相关配置请参考：www.axios-js.com/zh-cn/docs/#axios-request-config-1
