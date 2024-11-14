72d1e503d8005d5596a039367df5bd1ddaaa92eb
zoujunqin <1171485179@qq.com>
2024-11-14 10:29:55 +0800

    fix: 修复组件内多个跟节点，外部传递class，style导致的警告

--- src/pages/portal/components/person/index.vue
+++ src/pages/portal/components/person/index.vue
@@ -1,123 +1,130 @@
 <template>
-  <ProModal
-    ref="logoutModalRef"
-    confirm-button-text="确认"
-    content="确认退出登录?"
-    title="退出登录"
-    @confirm="handleLogoutConfirm"
-  >
-    <template #image>
-      <image
-        :src="getAssetsResource('@http/person/log-out-img.png')"
-        class="hx-w-full hx-h-[94px]"
-      />
-    </template>
-  </ProModal>
-
-  <ProModal
-    ref="contactModalRef"
-    confirm-button-text="联系我们"
-    content="0591 - 87809851"
-    title="联系我们"
-    @confirm="handleContactConfirm('0591 - 87809851')"
-  >
-    <template #image>
-      <image
-        :src="getAssetsResource('@http/person/connection-bg.png')"
-        class="hx-w-full hx-h-[116px]"
-      />
-    </template>
-  </ProModal>
-
-  <view
-    :style="`background-image: url(${getAssetsResource(
-      '@http/person/nav-bar-bg.svg'
-    )});`"
-    class="page-pt-with-navbar hx-pl-[8px] hx-pr-[8px] hx-bg-[length:100%_432px] hx-bg-no-repeat"
-  >
-    <view
-      class="hx-flex hx-items-center hx-justify-between hx-pl-[8px] hx-pr-[8px] hx-mt-[12px] hx-mb-[20px]"
+  <view>
+    <ProModal
+      ref="logoutModalRef"
+      confirm-button-text="确认"
+      content="确认退出登录?"
+      title="退出登录"
+      @confirm="handleLogoutConfirm"
     >
-      <view class="hx-flex hx-flex-1" @click="handleToLoginPage">
-        <view class="img-box">
-          <image
-            :src="getAssetsResource('@http/person/avatar-default.svg')"
-            class="hx-w-[54px] hx-h-[54px] hx-rounded-full"
-          />
-        </view>
-        <view class="hx-flex hx-flex-col hx-justify-center">
-          <text
-            class="hx-mb-[4px] hx-text-color-title hx-text-font-size-title hx-font-[600] hx-leading-[28px]"
-          >
-            {{ token ? userInfo?.workerName : '未登录' }}
-          </text>
-          <text
-            class="hx-text-text-color hx-text-font-size-base hx-leading-[20px] hx-font-[500]"
-          >
-            {{ token ? userInfo?.mobile : '点击登录，查看更多信息' }}
-          </text>
-        </view>
-      </view>
-      <view
-        class="hx-h-[30px] hx-w-[98px] hx-flex hx-items-center hx-justify-between hx-p-[8px_16px] hx-rounded-[15px] hx-shadow-button"
-        v-if="token"
-        @click="logout"
-      >
+      <template #image>
         <image
-          :src="getAssetsResource('@http/person/exit.svg')"
-          class="hx-w-[14px] hx-h-[14px] hx-mr-[4px]"
+          :src="getAssetsResource('@http/person/log-out-img.png')"
+          class="hx-w-full hx-h-[94px]"
         />
-        <text class="hx-text-font-size-sm hx-leading-[20px] hx-text-text-color">
-          退出登录
-        </text>
-      </view>
-    </view>
+      </template>
+    </ProModal>
+
+    <ProModal
+      ref="contactModalRef"
+      confirm-button-text="联系我们"
+      content="0591 - 87809851"
+      title="联系我们"
+      @confirm="handleContactConfirm('0591 - 87809851')"
+    >
+      <template #image>
+        <image
+          :src="getAssetsResource('@http/person/connection-bg.png')"
+          class="hx-w-full hx-h-[116px]"
+        />
+      </template>
+    </ProModal>
 
     <view
-      class="hx-h-[88px] hx-p-[16px] hx-flex hx-items-center hx-justify-between hx-bg-white hx-rounded-[8px] hx-shadow-card hx-mb-[10px]"
+      :style="`background-image: url(${getAssetsResource(
+        '@http/person/nav-bar-bg.svg'
+      )});`"
+      class="page-pt-with-navbar hx-pl-[8px] hx-pr-[8px] hx-bg-[length:100%_432px] hx-bg-no-repeat"
     >
       <view
-        v-for="(item, index) in panelItemList"
-        :key="index"
-        class="hx-h-[56px] hx-flex hx-flex-col hx-items-center"
-        @click="panelItemMap[item.type]?.handler"
+        class="hx-flex hx-items-center hx-justify-between hx-pl-[8px] hx-pr-[8px] hx-mt-[12px] hx-mb-[20px]"
       >
-        <view class="hx-relative">
-          <image :src="item.icon" class="hx-w-[36px] hx-h-[36px] hx-mb-[8px]" />
-          <ProBadge :value="item.badge" absolute />
+        <view class="hx-flex hx-flex-1" @click="handleToLoginPage">
+          <view class="img-box">
+            <image
+              :src="getAssetsResource('@http/person/avatar-default.svg')"
+              class="hx-w-[54px] hx-h-[54px] hx-rounded-full"
+            />
+          </view>
+          <view class="hx-flex hx-flex-col hx-justify-center">
+            <text
+              class="hx-mb-[4px] hx-text-color-title hx-text-font-size-title hx-font-[600] hx-leading-[28px]"
+            >
+              {{ token ? userInfo?.workerName : '未登录' }}
+            </text>
+            <text
+              class="hx-text-text-color hx-text-font-size-base hx-leading-[20px] hx-font-[500]"
+            >
+              {{ token ? userInfo?.mobile : '点击登录，查看更多信息' }}
+            </text>
+          </view>
         </view>
-
-        <text
-          class="hx-text-font-size-base hx-text-color-title hx-font-[500] hx-leading-[20px]"
+        <view
+          class="hx-h-[30px] hx-w-[98px] hx-flex hx-items-center hx-justify-between hx-p-[8px_16px] hx-rounded-[15px] hx-shadow-button"
+          v-if="token"
+          @click="logout"
         >
-          {{ item.name }}
-        </text>
+          <image
+            :src="getAssetsResource('@http/person/exit.svg')"
+            class="hx-w-[14px] hx-h-[14px] hx-mr-[4px]"
+          />
+          <text
+            class="hx-text-font-size-sm hx-leading-[20px] hx-text-text-color"
+          >
+            退出登录
+          </text>
+        </view>
       </view>
-    </view>
 
-    <view
-      class="hx-bg-white hx-rounded-[8px] hx-pl-[12px] hx-pr-[12px] hx-mb-[10px] hx-pt-[16px]"
-    >
-      <vertical-list-item
-        v-for="(item, index) in firstVerticalList"
-        :key="index"
-        :data="item"
-        :listLength="firstVerticalList.length"
-        class="hx-pb-[16px]"
-        @click="verticalListItemMap[item.type]?.handler"
-      />
-    </view>
-    <view
-      class="hx-bg-white hx-rounded-[8px] hx-pl-[12px] hx-pr-[12px] hx-pt-[16px]"
-    >
-      <vertical-list-item
-        v-for="(item, index) in secondVerticalList"
-        :key="index"
-        :data="item"
-        :listLength="secondVerticalList.length"
-        class="hx-pb-[16px]"
-        @click="verticalListItemMap[item.type]?.handler"
-      />
+      <view
+        class="hx-h-[88px] hx-p-[16px] hx-flex hx-items-center hx-justify-between hx-bg-white hx-rounded-[8px] hx-shadow-card hx-mb-[10px]"
+      >
+        <view
+          v-for="(item, index) in panelItemList"
+          :key="index"
+          class="hx-h-[56px] hx-flex hx-flex-col hx-items-center"
+          @click="panelItemMap[item.type]?.handler"
+        >
+          <view class="hx-relative">
+            <image
+              :src="item.icon"
+              class="hx-w-[36px] hx-h-[36px] hx-mb-[8px]"
+            />
+            <ProBadge :value="item.badge" absolute />
+          </view>
+
+          <text
+            class="hx-text-font-size-base hx-text-color-title hx-font-[500] hx-leading-[20px]"
+          >
+            {{ item.name }}
+          </text>
+        </view>
+      </view>
+
+      <view
+        class="hx-bg-white hx-rounded-[8px] hx-pl-[12px] hx-pr-[12px] hx-mb-[10px] hx-pt-[16px]"
+      >
+        <vertical-list-item
+          v-for="(item, index) in firstVerticalList"
+          :key="index"
+          :data="item"
+          :listLength="firstVerticalList.length"
+          class="hx-pb-[16px]"
+          @click="verticalListItemMap[item.type]?.handler"
+        />
+      </view>
+      <view
+        class="hx-bg-white hx-rounded-[8px] hx-pl-[12px] hx-pr-[12px] hx-pt-[16px]"
+      >
+        <vertical-list-item
+          v-for="(item, index) in secondVerticalList"
+          :key="index"
+          :data="item"
+          :listLength="secondVerticalList.length"
+          class="hx-pb-[16px]"
+          @click="verticalListItemMap[item.type]?.handler"
+        />
+      </view>
     </view>
   </view>
 </template>
